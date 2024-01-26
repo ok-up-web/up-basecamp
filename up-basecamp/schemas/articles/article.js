21ad2516bb4012e75cc9ec195eed583bcd1636de
Anders Bonnevier <anders@upptakke.se>
2024-01-26 14:58:59 +0100

    Update article.js

--- up-basecamp/schemas/articles/article.js
+++ up-basecamp/schemas/articles/article.js
@@ -56,49 +56,7 @@ export default {
                 { type: 'imageTextBlock'}
         ],
       },
-      {
-        name: 'body',
-        title: 'Body',
-        type: 'array',
-        of: [
-          {
-            type: 'block',
-            styles: [
-              { title: 'Normal', value: 'normal' },
-              { title: 'H1', value: 'h1' },
-              { title: 'H2', value: 'h2' },
-              { title: 'H3', value: 'h3' },
-              { title: 'H4', value: 'h4' },
-              { title: 'H5', value: 'h5' }
-            ],
-            lists: [
-              { title: 'Bullet', value: 'bullet' },
-              { title: 'Numbered', value: 'number' }
-            ],
-            marks: {
-              decorators: [
-                { title: 'Strong', value: 'strong' },
-                { title: 'Emphasis', value: 'em' }
-              ],
-              annotations: [
-                {
-                  name: 'link',
-                  type: 'object',
-                  title: 'URL',
-                  fields: [
-                    {
-                      name: 'href',
-                      type: 'url',
-                      title: 'URL'
-                    }
-                  ]
-                }
-              ]
-            }
-          },
-          { type: 'image' }
-        ],
-      },
+       
 
           {
             name: 'image',
